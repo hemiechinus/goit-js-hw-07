e8660855cf4772b5eb0f3083cff8a6feb95f2e7d
Volodymyr Kovalenko <komp215.16@gmail.com>
2021-09-22 21:14:12 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ulList = document.getElementById('ingredients')
+
+const elements = ingredients.map(value => {
+    const element = document.createElement('li');
+    element.textContent = value;
+    return element    
+})
+
+ulList.append(...elements);
